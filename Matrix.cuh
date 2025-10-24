feb5e41ebeba236d01d908571a0b1f950798383e
HosseinAlbakri <hosseinalbakri3@gmail.com>
2025-10-24 14:12:29 -0400

    added the constant size

--- Matrix.cuh
+++ Matrix.cuh
@@ -5,6 +5,11 @@
 
 using namespace std;
 
+// Global project configuration constants
+// Inline constexpr gives external linkage and avoids multiple-definition
+// issues when included from many translation units.
+inline constexpr int BLOCK_SIZE = 16;
+
 class Matrix {
 public:
     half *data = nullptr;
